ac8e0aa0d913812946ea18a198b5b7c76d22be98
David Mackey <davidmackey92@gmail.com>
2023-03-06 00:50:10 -0600

    html

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -3,6 +3,22 @@ const Intern = require("../../lib/intern.js");
 
 const generateTeam = (team) => {
 
+  const managerCard = (man) => {
+    return `
+  <div class="p-3 m-5 card shadow bg-white border border-2 border-primary" style="width: 20vw;">
+          <div class="card p-3 mb-3 bg-primary bg-opacity-0 text-white border border-primary">
+            <h2 class="display-5 text-center">${man.getName()}</h2>
+            <h3 class="text-black text-center"><i class="fa-solid fa-mug-hot m-2"></i> ${man.getRole()}</h3>
+          </div>
+          <div class="card-body">
+            <p class="p-3 bg-light border rounded border-primary">ID: ${man.getId()}</p>
+            <p class="p-3 bg-light border rounded border-primary">Email: <a href="mailto:${man.getEmail()}">${man.getEmail()}</a></p>
+            <p class="p-3 bg-light border rounded border-primary">Office Number: <a href="tel:${man.getOfficeNumber()}">${man.getOfficeNumber()}</a></p>
+          </div>
+        </div>
+  `;
+  };
+
   const engineerCard = (eng) => {
     return `
       <div class="p-3 m-5 card shadow bg-white border border-2 border-primary" style="width: 20vw;">
@@ -11,9 +27,9 @@ const generateTeam = (team) => {
           <h3 class="text-black text-center"><i class="fa-solid fa-mug-hot m-2"></i> ${eng.getRole()}</h3>
         </div>
         <div class="card-body">
-          <p class="p-2 m-1 bg-light border rounded border-primary">ID: ${eng.getId()}</p>
-          <p class="p-2 m-1 bg-light border rounded border-primary">Email: <a href="${eng.getEmail()}">${eng.getEmail()}</a></p>
-          <p class="p-2 m-1 bg-light border rounded border-primary">GitHub: <a href="https://github.com/${eng.getGitHub()}" target="_blank" rel="noopener noreferrer">${eng.getGitHub()}</a>
+          <p class="p-3 bg-light border rounded border-primary">ID: ${eng.getId()}</p>
+          <p class="p-3 bg-light border rounded border-primary">Email: <a href="mailto:${eng.getEmail()}">${eng.getEmail()}</a></p>
+          <p class="p-3 bg-light border rounded border-primary">GitHub: <a href="https://github.com/${eng.getGitHub()}" target="_blank">${eng.getGitHub()}</a>
           </p>
         </div>
       </div>
@@ -22,31 +38,15 @@ const generateTeam = (team) => {
 
   const internCard = (intern) => {
     return `
-<div class="p-3 m-2 card shadow bg-secondary bg-opacity-25 border border-primary" style="width: 18rem;">
-        <div class="card p-3 mb-2 bg-primary bg-opacity-75 text-white border border-primary">
-          <h2>${intern.getName()}</h2>
-          <h3>${intern.getRole()} 🎸</h3>
+<div class="p-3 m-5 card shadow bg-white border border-2 border-primary" style="width: 20vw;">
+        <div class="card p-3 mb-3 bg-primary bg-opacity-0 text-white border border-primary">
+          <h2 class="display-5 text-center">${intern.getName()}</h2>
+          <h3 class="text-black text-center"><i class="fa-solid fa-hand-back-fist m-2"></i> ${intern.getRole()}</h3>
         </div>
         <div class="card-body">
-          <p class="p-2 m-1 bg-light border rounded border-primary">ID: ${intern.getId()}</p>
-          <p class="p-2 m-1 bg-light border rounded border-primary">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
-          <p class="p-2 m-1 bg-light border rounded border-primary">School: ${intern.getSchool()}</p>
-        </div>
-      </div>
-`;
-  };
-
-  const managerCard = (man) => {
-    return `
-<div class="p-3 m-2 card shadow bg-secondary bg-opacity-25 border border-primary" style="width: 18rem;">
-        <div class="card p-3 mb-2 bg-primary bg-opacity-75 text-white border border-primary">
-          <h2>${man.getName()}</h2>
-          <h3>${man.getRole()} ☕️</h3>
-        </div>
-        <div class="card-body">
-          <p class="p-2 m-1 bg-light border rounded border-primary">ID: ${man.getId()}</p>
-          <p class="p-2 m-1 bg-light border rounded border-primary">Email: <a href="mailto:${man.getEmail()}">${man.getEmail()}</a></p>
-          <p class="p-2 m-1 bg-light border rounded border-primary">Office Number: ${man.getOfficeNumber()}</p>
+          <p class="p-3 bg-light border rounded border-primary">ID: ${intern.getId()}</p>
+          <p class="p-3 bg-light border rounded border-primary">Email: <a href="mailto:${intern.getEmail()}">${intern.getEmail()}</a></p>
+          <p class="p-3 bg-light border rounded border-primary">School: ${intern.getSchool()}</p>
         </div>
       </div>
 `;
@@ -100,6 +100,7 @@ module.exports = (team) => {
     </header>
     <!-- ! -->
     <main class="d-flex flex-wrap justify-content-center">
+      <!-- ! -->
         ${generateTeam(team)}
     </main>
 
