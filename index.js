501295429f7665a5b8eb648574c90b95576f5900
David Mackey <davidmackey92@gmail.com>
2023-03-07 21:04:47 -0600

    change to output folder and related js link

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ const app = {
     function writeToFile(fileName, data) {
       return fs.writeFileSync(path.join(process.cwd(), fileName), data);
     }
-    writeToFile("./dist/team.html", generateTeam(teamArray));
+    writeToFile("./output/team.html", generateTeam(teamArray));
     console.log(`
       Team profile generated!
       `);
